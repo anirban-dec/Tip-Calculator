13a22a9ab8b98c257481e1004efb0564e7caeb7d
Anirban <103098247+anirban-dec@users.noreply.github.com>
2022-08-22 19:17:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,28 @@
 
+const calc=()=>{
+
+    let amount=document.getElementById("bill-amount").value;
+    if(amount=="" ||amount==0){
+        alert("You Have Not Mentioned 'Bill Amount'")
+    }
+    
+    let tipPer=document.getElementById("tip-perc").value;
+    if(tipPer=="" || tipPer==0){
+        confirm("Continue Without giving Any Tip?")
+    }
+
+    let tip=amount*( tipPer/100);
+    tip=tip.toFixed(2);
+    
+    let total=Number (tip)+ Number (amount);
+    
+    
+
+
+   document.getElementById("tip-amount").value=tip;
+   
+   
+
+   document.getElementById("total-bill").value=total;
+
+}
